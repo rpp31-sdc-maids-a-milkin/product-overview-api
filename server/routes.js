6d7dfa4f515899ffdbb4a5aff3720e2f799418c1
xoxohorses <69382434+xoxohorses@users.noreply.github.com>
2021-12-12 15:03:19 -0800

    Using supertest to test the http server

--- server/routes.js
+++ server/routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.status(200).send('Hello World')
 })
 
-export default app
+module.exports = app
--- server/server.js
+++ server/server.js
@@ -1,3 +1,3 @@
-import app from 'routes.js'
+const app = require('routes.js')
 
 app.listen(3001, () => console.log('Listening on port 3001'))
