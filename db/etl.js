3de17e6c579eeae5ebe425adf99566c61ee06053
xoxohorses <69382434+xoxohorses@users.noreply.github.com>
2021-12-14 22:42:44 -0800

    script for parsing CSV and attaching columns started

---
+++ db/etl.js
@@ -0,0 +1,76 @@
+const csv = require('csv-parse')
+const fs = require('fs')
+
+const products = []
+const styles = []
+const skus = []
+
+parseCSV('products', products)
+  .then(() => {
+    attachProperty('features', products, attachFeatures)
+  })
+
+function parseCSV (file, storage) {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(`data/${file}.csv`)
+      .pipe(csv.parse({
+        delimiter: ',',
+        columns: true,
+        relax_quotes: true,
+        relax_column_count: true
+      }))
+      .on('data', function (row) {
+        const index = row.id
+        storage[index] = row
+      })
+      .on('error', function (error) {
+        console.error(`${file}.csv: `, error.message)
+        reject(error)
+      })
+      .on('end', function () {
+        console.log(`${file}.csv has been parsed`)
+        resolve()
+      })
+  })
+}
+
+function attachProperty (file, storage, customAttachFunc) {
+  fs.createReadStream(`data/${file}.csv`)
+    .pipe(csv.parse({
+      delimiter: ',',
+      columns: true,
+      relax_quotes: true,
+      relax_column_count: true
+    }))
+    .on('data', function (row) {
+      customAttachFunc(row)
+    })
+    .on('error', function (error) {
+      console.error(`${file}.csv: `, error.message)
+    })
+    .on('end', function () {
+      console.log(storage)
+      console.log(`${file}.csv has been parsed`)
+    })
+}
+
+function attachFeatures (row) {
+  const index = row.product_id
+  const product = products[index]
+  const feature = {
+    feature: row.feature,
+    value: row.value
+  }
+
+  if (!product.features) {
+    product.features = [feature]
+  } else {
+    product.features.push(feature)
+  }
+}
+
+// get products into array
+// then add features to corresponding product objects
+
+// get styles into array
+// then add photos to corresponding style objects
