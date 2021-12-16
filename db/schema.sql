4552bff7f245c63ba4a218b869a7bb782da5fe12
xoxohorses <69382434+xoxohorses@users.noreply.github.com>
2021-12-16 00:48:57 -0800

    Improved schema design/scripts

--- db/schema.sql
+++ db/schema.sql
@@ -4,31 +4,39 @@ CREATE DATABASE atelier;
 
 \c atelier;
 
-CREATE TABLE product_info (
+-- create tables
+CREATE TABLE products (
   product_id integer PRIMARY KEY,
   product_name varchar(255),
   product_slogan text,
   product_description text,
   product_category varchar(255),
   product_default_price money,
-  product_features jsonb,
-  product_created_at timestamp,
-  product_updated_at timestamp
+  product_features jsonb
 );
 
-CREATE TABLE style_info (
+CREATE TABLE styles (
   style_id integer PRIMARY KEY,
-  product_id integer REFERENCES product_info,
+  product_id integer,
   style_name varchar(255),
+  style_sale_price text,
   style_original_price money,
-  style_sale_price money,
   style_default integer,
   style_photos jsonb
 );
 
-CREATE TABLE sku_info (
+CREATE TABLE skus (
   sku_id integer PRIMARY KEY,
-  style_id integer REFERENCES style_info,
+  style_id integer,
   sku_size varchar(5),
   sku_quantity integer
 );
+
+-- load data
+\copy products FROM '/Users/julieyu/Documents/hack-reactor/product-overview-api/data/product_info.csv' delimiter ',' csv header
+\copy styles FROM '/Users/julieyu/Documents/hack-reactor/product-overview-api/data/style_info.csv' delimiter ',' csv header
+\copy skus FROM '/Users/julieyu/Documents/hack-reactor/product-overview-api/data/sku_info.csv' delimiter ',' csv header
+
+-- append timestamp info to product table afterwards
+ALTER TABLE products ADD COLUMN product_created_at timestamp DEFAULT NOW();
+ALTER TABLE products ADD COLUMN product_updated_at timestamp DEFAULT NOW();
