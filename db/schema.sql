5e7a338375734555a6a83ef712272650da01125e
xoxohorses <69382434+xoxohorses@users.noreply.github.com>
2021-12-17 01:12:20 -0800

    Less verbose column names in schema design

--- db/schema.sql
+++ db/schema.sql
@@ -5,29 +5,29 @@ CREATE DATABASE atelier;
 -- create tables
 CREATE TABLE products (
   product_id integer PRIMARY KEY,
-  product_name varchar(255),
-  product_slogan text,
-  product_description text,
-  product_category varchar(255),
-  product_default_price money,
-  product_features jsonb
+  name varchar(255),
+  slogan text,
+  description text,
+  category varchar(255),
+  default_price money,
+  features jsonb
 );
 
 CREATE TABLE styles (
   style_id integer PRIMARY KEY,
   product_id integer,
-  style_name varchar(255),
-  style_sale_price text,
-  style_original_price money,
-  style_default integer,
-  style_photos jsonb
+  name varchar(255),
+  sale_price text,
+  original_price money,
+  default_style integer,
+  photos jsonb
 );
 
 CREATE TABLE skus (
   sku_id integer PRIMARY KEY,
   style_id integer,
-  sku_size varchar(255),
-  sku_quantity integer
+  size varchar(255),
+  quantity integer
 );
 
 -- load data
