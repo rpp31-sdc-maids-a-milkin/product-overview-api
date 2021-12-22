ec1106e23e70cbbcd1b1f58e24ad571ed421ed9d
xoxohorses <69382434+xoxohorses@users.noreply.github.com>
2021-12-21 20:17:48 -0800

    set up all backend endpoints

--- server/endpoints.js
+++ server/endpoints.js
@@ -2,9 +2,27 @@ const express = require('express')
 const db = require('../db/db.js')
 const app = express()
 
-app.get('/', async function (req, res) {
+app.get('/products', async function (req, res) {
   try {
     const result = await db.getProducts()
+    res.status(200).send(result)
+  } catch (error) {
+    res.status(500).send({ error })
+  }
+})
+
+app.get('/products/:productId', async function (req, res) {
+  try {
+    const result = await db.getProduct()
+    res.status(200).send(result)
+  } catch (error) {
+    res.status(500).send({ error })
+  }
+})
+
+app.get('/products/:productId/styles', async function (req, res) {
+  try {
+    const result = await db.getStyles()
     res.status(200).send(result)
   } catch (error) {
     res.status(500).send({ error })
