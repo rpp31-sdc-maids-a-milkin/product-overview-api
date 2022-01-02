e47ad68435c7057a91d449777fa0e423a82e356d
xoxohorses <69382434+xoxohorses@users.noreply.github.com>
2022-01-02 15:30:26 -0800

    K6 stress testing set up for all endpoints

--- performance/products.js
+++ performance/products.js
@@ -2,7 +2,7 @@ import http from 'k6/http'
 import { sleep } from 'k6'
 
 export const options = {
-  vus: 154,
+  vus: 155,
   duration: '30s'
 }
 
@@ -11,8 +11,8 @@ export function setup () {
 }
 
 export default function () {
-  const page = Math.floor(Math.random() * 100011) + 50000
-  const count = Math.floor(Math.random() * 10) + 5
+  const page = Math.floor(Math.random() * (100011 - 50000) + 50000)
+  const count = Math.floor(Math.random() * (10 - 5) + 5)
   http.get('http://127.0.0.1:3001/products', { params: { page, count } })
   sleep(1)
 }
