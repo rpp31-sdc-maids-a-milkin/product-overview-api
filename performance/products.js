a7a29be64fd4d587e2d96c141b7beb3f457cc536
xoxohorses <69382434+xoxohorses@users.noreply.github.com>
2022-01-02 13:52:35 -0800

    K6 set up for /products endpoint

---
+++ performance/products.js
@@ -0,0 +1,18 @@
+import http from 'k6/http'
+import { sleep } from 'k6'
+
+export const options = {
+  vus: 154,
+  duration: '30s'
+}
+
+export function setup () {
+  console.log('DATE: ', new Date())
+}
+
+export default function () {
+  const page = Math.floor(Math.random() * 100011) + 50000
+  const count = Math.floor(Math.random() * 10) + 5
+  http.get('http://127.0.0.1:3001/products', { params: { page, count } })
+  sleep(1)
+}
